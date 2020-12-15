c3e2fd9d63087022d799a6d5b5529738eca9c5fc
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-12-15 12:53:13 +0700

    Changed params in api

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from nlp_py.similarity import get_similarity
 from nlp_py.main import get_similarity_stat
 app = Flask(__name__)
 
-
-arr_trust = ['true', '1', 't', 'y', 'yes']
+text_param_error = 'Bad request! (you need to add `text` param)'
+param_error_code = 400
 
 @app.route('/')
 def index():
@@ -18,24 +18,18 @@ def nlp_words():
         append = args.get('is-append')
         is_append = False
 
-        if append and append in arr_trust: is_append = True
+        if append and append in ['true', '1', 't', 'y', 'yes']: is_append = True
 
         return jsonify(get_similarity(args.get('text'), is_append))
 
-    return 'You need to add `text` param'
+    return text_param_error, param_error_code
 
 @app.route('/nlp-computing')
 def nlp_computing():
     args = request.args
-    if args and args.get('text'):
-        append = args.get('is-append')
-        is_append = False
 
-        if append and append in arr_trust: is_append = True
-
-        return jsonify(get_similarity_stat(args.get('text'), is_append))
-
-    return 'You need to add `text` param'
+    return jsonify(get_similarity_stat(args.get('text'))) if args and args.get('text') \
+        else text_param_error, param_error_code
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- nlp_py/main.py
+++ nlp_py/main.py
@@ -1,4 +1,4 @@
 from nlp_py.similarity import get_similarity
 
-def get_similarity_stat(text, is_append = False):
-    return get_similarity(text, is_append)
+def get_similarity_stat(text):
+    return get_similarity(text, False)
