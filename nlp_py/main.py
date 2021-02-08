88747b4281d8ff5dfd665ea3172cf6a5f332c8ab
Shamil Qurbonov <qurbonovshamil@gmail.com>
2021-02-08 16:20:05 +0700

    Changed logic

--- nlp_py/main.py
+++ nlp_py/main.py
@@ -1,4 +1,4 @@
-# import math
+import math
 import pandas as pd
 import nlp_py.constants as constants
 from nlp_py.similarity import get_similarity
@@ -10,11 +10,14 @@ titles = df['name'].str.lower()
 
 
 def get_similarity_date(text, user_id=None):
-    text = str(sub(r'[^\w]|[^\D]', ' ', text).lower()).strip()  # TODO:: MAYBE CHANGE IT after MYSQL!!!
-    nlp_words = get_similarity(text, True)
+    max_count_words = len(text.split())
+    text_nlp = str(sub(r'[^\w]|[^\D]', ' ', text).lower()).strip()  # TODO:: MAYBE CHANGE IT after MYSQL!!!
+    nlp_words = get_similarity(text_nlp, True)
+
+    for numSym in str(sub(r'[^\d]+', ' ', text)).strip().split():   # adding number symbols
+        nlp_words.append(numSym)
 
     tasks = []
-    tmp = []
 
     # try to find from nlp_words and add ratings for words
     for idx, words in enumerate(titles):
@@ -22,21 +25,17 @@ def get_similarity_date(text, user_id=None):
         countSimilarity = [nlp_word in norm_text for nlp_word in nlp_words].count(True)
 
         # Если в задачи меньше слов чем в MIN_COUNT_WORDS, то данная переменная уменьшается на 1.
-        if constants.MIN_COUNT_WORDS == countSimilarity or len(text.split()) < constants.MIN_COUNT_WORDS and constants.MIN_COUNT_WORDS - 1 == countSimilarity:
-            if idx in tmp:
-                for task_idx, [index, _] in enumerate(tasks):
-                    if index == idx:
-                        tasks[task_idx][1] += constants.RATING_WORD_SIMILARITY  # [task_idx][1] - it's a rating!
-            else:
-                tmp.append(idx)
-                tasks.append([idx, 1])
+        if constants.MIN_COUNT_WORDS <= countSimilarity or max_count_words < constants.MIN_COUNT_WORDS and constants.MIN_COUNT_WORDS - 1 <= countSimilarity:
+            tasks.append([idx, countSimilarity * constants.RATING_WORD_SIMILARITY])
+
+    sorted_tasks = sorted(tasks, key=lambda x: x[1])
 
     data = []
 
     # adding data
     total_ratings = 0
 
-    for idx, rating in tasks:
+    for idx, rating in sorted_tasks[-constants.SAMPLE_COUNT:]:
         owner_id, times = df.loc[idx, ['owner_id', 'times']]
         # space for adding params
 
@@ -55,13 +54,13 @@ def get_similarity_date(text, user_id=None):
     mx = int(stat.math_expectation_x(data))
 
     # Дисперсия случайной величины
-    # dx = int(stat.variance_x(data, mx))
+    dx = int(stat.variance_x(data, mx))
 
     # Среднеквадратичесвое оклонение случайной величины для задачи
-    # gx = int(math.sqrt(dx))
+    gx = int(math.sqrt(dx))
 
-    # return {'mx': display_time(mx), 'dx': display_time(dx), 'gx': display_time(gx)}
-    return {'mx': display_time(mx)}
+    return {'mx': display_time(mx), 'dx': display_time(dx), 'gx': display_time(gx)}
+    # return {'mx': display_time(mx), 'tasks_count': len(tasks), 'data': data}
 
 
 # Function for convert seconds to days, hours and minutes
